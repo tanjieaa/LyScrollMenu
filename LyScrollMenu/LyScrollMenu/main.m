5b1c8d077169811165a409b63f077d3fc3d9de42
Lying <lying_555@163.com>
2016-06-28 02:40:01 +0800

    首次提交

---
+++ LyScrollMenu/LyScrollMenu/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LyScrollMenu
+//
+//  Created by Lying on 16/6/27.
+//  Copyright © 2016年 Lying. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
